97413f85b121859ed656d1284bcb2694488c550f
andyying <andy.ying@hotmail.com>
2021-10-16 17:23:24 +0200

    Mobile first

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ import KaomojiList from './components/KaomojiList/KaomojiList';
 const App = () => {
   return (
     <div>
+      <h1>Kaomojis</h1>
+      <h2>
+        Emoticons that use Japanese characters, Latin letters and
+        punctuation marks to represent facial expressions that convey
+        and emotion. 
+      </h2>
+      <h3>
+        Click the Kaomoji you'd like to use and it will copy it to your
+        clipboard.
+      </h3>
       <KaomojiList />
     </div>
   );
