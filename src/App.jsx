f2b174b404b99a2ab137938b01ebc46566eb72e8
andyying <andy.ying@hotmail.com>
2021-10-19 23:51:59 +0200

    Updated favicon and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header/Header';
 import KaomojiList from './components/KaomojiList/KaomojiList';
 import Footer from './components/Footer/Footer';
 import './App.css'
+import ReactGA from 'react-ga'
 
 const App = () => {
+  ReactGA.initialize('G-KHRSM3RLPY');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
+  useEffect(() => {
+    document.title = "Kaomojicons"
+  }, [])
+
   return (
     <div className="grid-container">
       <Header />
@@ -19,8 +27,3 @@ const App = () => {
 };
 
 export default App;
-
-/**
-
-
-*/
